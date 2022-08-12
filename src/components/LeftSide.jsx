c2f7611219fd05f9bdb20da7ff1c38bcd717a082
Elena <elena_tapean@yahoo.com>
2022-08-12 11:28:28 +0300

    added UserInfo Context

--- src/components/LeftSide.jsx
+++ src/components/LeftSide.jsx
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 import { ResumeContext } from '../context/resume';
 import './css/left-side.scss';
 import { TextComponent } from './TextComponent';
+import { UserInfo } from './UserInfo';
 
 export const LeftSide = function () {
     const sections = ['about', 'work', 'soft_skills', 'special_skills']
@@ -9,11 +10,7 @@ export const LeftSide = function () {
 
     return (
         <section className="left-side">
-            <p className="contact-details">
-            Call {data.phone} or
-            <br/>
-            Email {data.email}
-            </p>
+            <UserInfo />
 
             {sections.map(key => (
                 <TextComponent key={key} 
---
+++ src/components/UserInfo.jsx
@@ -0,0 +1,14 @@
+import { useContext } from "react";
+import { ResumeContext } from "../context/resume";
+
+export const UserInfo = function () {
+    const data = useContext(ResumeContext);
+
+    return (
+        <p className="contact-details">
+            Call {data.phone} or
+            <br/>
+            Email {data.email}
+        </p>
+    )
+}
