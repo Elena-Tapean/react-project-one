2acded590d0ff15db2127d71269f44854c092e9c
Elena <elena_tapean@yahoo.com>
2022-08-14 17:04:31 +0300

    responsive footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,20 @@
 import './css/footer-bar.scss';
 
+function handleSubmit (event) {
+    event.preventDefault();
+}
+
 export const Footer = function () {
     return (
         <div className='page-container'>
             <footer className='footer-bar'>
                 <nav>
                     <form>
-                        <input type="text" placeholder="Search this site" id="search" />
-                        <button type="submit">Search</button>
+                        <input type="text" placeholder="Search..." id="search" />
+                        <button onSubmit={handleSubmit} type="submit">Search</button>
                     </form>
-                    <span>© 2022 Elena Țăpean. All Rights Reserved.</span>
+                    <span>© Copyright 2022</span>
+                    <span className='span-larger'>© Copyright 2022 Elena Țăpean. All Rights Reserved.</span>
                 </nav>
             </footer>
         </div>
