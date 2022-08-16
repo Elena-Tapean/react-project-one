4318eeaf3d63dabea31e8ecfb4ffec32c5bcca89
Elena <elena_tapean@yahoo.com>
2022-08-16 16:22:47 +0300

    small changes to Homepage

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -10,16 +10,16 @@ export const Home = function () {
                 <p>This here is my first project. I have used React.js to create it.</p>
                 <h3>Where to go from here?</h3>
                 <ul>
-                    <li>To learn more about me, visit <a href="http://localhost:3000/resume/elena/">the Resume page</a>.</li>
-                    <li>To view my portfolio, visit <a href="http://localhost:3000/portfolio">the Portfolio page</a>.</li>
-                    <li>To contact me, visit <a href="http://localhost:3000/contact">the Contact page</a>.</li>
+                    <li>To learn more about me, visit <a href="http://localhost:3000/resume/elena/">the Resume page</a></li>
+                    <li>To view my portfolio, visit <a href="http://localhost:3000/portfolio">the Portfolio page</a></li>
+                    <li>To contact me, visit <a href="http://localhost:3000/contact">the Contact page</a></li>
                 </ul>
                 <h3>What do I do beside code?</h3>
                 <p>I love playing games, writing poetry and taking photos.</p>
                 <ul>
-                    <li>Visit <a href="https://gurushots.com/elena_tapean/photos"> Gurushots profile</a>.</li>
-                    <li>Visit <a href="https://steamcommunity.com/id/elena_tapean"> Steam profile</a>.</li>
-                    <li>Visit <a href="https://elenatapeanpoetry.wordpress.com/"> Poetry blog</a>.</li>
+                    <li>Visit <a href="https://gurushots.com/elena_tapean/photos"> Gurushots profile</a></li>
+                    <li>Visit <a href="https://steamcommunity.com/id/elena_tapean"> Steam profile</a></li>
+                    <li>Visit <a href="https://elenatapeanpoetry.wordpress.com/"> Poetry blog</a></li>
                 </ul>
             </div>
         </main>
