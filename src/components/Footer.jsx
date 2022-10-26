b0e4186467a613423a9d677ffa6eaa510df57b61
Elena <elena_tapean@yahoo.com>
2022-10-26 16:33:22 +0300

    changed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,10 @@
 import './css/footer-bar.scss';
 
-function handleSubmit (event) {
-    event.preventDefault();
-}
-
 export const Footer = function () {
     return (
         <div className='page-container'>
             <footer className='footer-bar'>
                 <nav>
-                    <form>
-                        <input type="text" placeholder="Search..." id="search" />
-                        <button onSubmit={handleSubmit} type="submit">Search</button>
-                    </form>
                     <span>© Copyright 2022</span>
                     <span className='span-larger'>© Copyright 2022 Elena Țăpean. All Rights Reserved.</span>
                 </nav>
