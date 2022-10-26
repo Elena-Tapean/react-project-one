dffe484d9d45d81e437a2f0f4ee08132deaf0a63
Elena <elena_tapean@yahoo.com>
2022-10-26 19:49:38 +0300

    changed contact page needs more

--- src/views/Portfolio.jsx
+++ src/views/Portfolio.jsx
@@ -5,12 +5,19 @@ export const Portfolio = function () {
         <main>
             <div className="port-folio">
                 <h1>My projects so far</h1>
-                <div className='container'>
-                    <a className='item' href="http://localhost:3000/resume/elena/" target='_blank'><img src="https://scontent-otp1-1.xx.fbcdn.net/v/t39.30808-6/297272427_8061666247206686_5854492312499706271_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeEKtT2g1FK6xrLWWxn__So4vh0sdALzl-u-HSx0AvOX63MUoUN1-S_zE0RQZckU0QBb-cl2IHJsBLgKPdYcnKmz&_nc_ohc=08h2ZdxtbNUAX9a9Ck_&tn=zBda7qO6N6VquDzN&_nc_ht=scontent-otp1-1.xx&oh=00_AT9-hm6QyS6kxFlGuxWe2V73hQwdWQM4Y8wBk8N-AROGjg&oe=6300C008" alt="React Resume" /></a>
-                    <a className='item' href="#"><img src="https://scontent-otp1-1.xx.fbcdn.net/v/t39.30808-6/296303611_8059018590804785_7478927585763227444_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeHLxu9junQYpdBsag9EuSVq73RPSAqsX3HvdE9ICqxfcS-KhD3ECN37wLZkwL72HnHr7zzs_uqi0p8othsICZ5r&_nc_ohc=z1NEaFIM2nkAX9VyNwO&tn=zBda7qO6N6VquDzN&_nc_ht=scontent-otp1-1.xx&oh=00_AT9BREppnXycBjij4w2XamWfXPxh6grWCaXsYPqjorZ-eQ&oe=6301E502" alt="Vue Website" /></a>
-                    <a className='item' href="https://elenatapeanpoetry.wordpress.com/" target='_blank'><img src="https://scontent-otp1-1.xx.fbcdn.net/v/t39.30808-6/297160933_8058832867490024_6575266018145341092_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeEZ2tGlRK6cFbnL24-UZhXfQSLyfoBKF0NBIvJ-gEoXQ6J28X_1zLiuVCkg_WhewjOZ_o4HKTlPoCgaOao0a3uv&_nc_ohc=Qu_hZa3Hp3MAX_i_UfE&_nc_oc=AQlA6LaOlPByZrdGsITzaf_6XsAZklVc55cizIi3zEuGQ_FQEDtNeiBP803PvaEdTNc&tn=zBda7qO6N6VquDzN&_nc_ht=scontent-otp1-1.xx&oh=00_AT_mIagi89zjRJzfXYmw4GyGUxRYhFynoT_SE0JoB9SClg&oe=6300DFF7" alt="Poetry Blog" /></a>
-                    <a className='item' href="https://elenatapeanphotos.wordpress.com/" target='_blank'><img src="https://scontent-otp1-1.xx.fbcdn.net/v/t39.30808-6/296364111_8058836417489669_3469951829364631981_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeFnmn5Mq2PajpqNexQ4sBN7a_XIxX0Fw6xr9cjFfQXDrK-3Kk1EqapA2wZD28QiCji-kI87kkOm9Bq4qjojaPzG&_nc_ohc=5MuPE9_AulgAX_AD7sq&_nc_ht=scontent-otp1-1.xx&oh=00_AT8UBlxmLyI7ZFOSFBE2p9lnSfSxaXruj4SMCKX3QLKx7A&oe=630157C3" alt="Photography Blog" /></a>
-                    <a className='item' href="#"><img src="https://scontent-otp1-1.xx.fbcdn.net/v/t39.30808-6/296391965_8059024344137543_121256030906353369_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeGdSnkvdRA_aAWfr9YrxRQJMNw-DLCuM34w3D4MsK4zfjP-dE-GLc6eav1gEDThaP9u_Jw65cGfWsN5cSxVkKEp&_nc_ohc=PPQkxa1IoV0AX_ezqF1&_nc_ht=scontent-otp1-1.xx&oh=00_AT_ROL28V2R3DMlL65QzX3qBJPtlYLOBTyKeTljQAkf18Q&oe=63015E4C" alt="A Simple Game" /></a>
+                <div className='min-flex'>
+                    <div>
+                        <a href="http://localhost:3000/resume/elena">React.js Resume</a>
+                        <a href="http://localhost:8080/">Vue.js Bookstore Website</a>
+                        <a href="https://docs.google.com/document/d/1sU5-jxUbbbeO97Z6UGM0VLGGrTFz2Lfo16tVPfRdgi0/edit">Game Story and Designs</a>
+                    </div>
+                </div>
+                <div className='flex-div'>
+                    <div>
+                        <a href="http://localhost:3000/resume/elena">React.js Resume</a>
+                        <a href="http://localhost:8080/">Vue.js Bookstore Website</a>
+                        <a href="https://docs.google.com/document/d/1sU5-jxUbbbeO97Z6UGM0VLGGrTFz2Lfo16tVPfRdgi0/edit">Game Story and Designs</a>
+                    </div>
                 </div>
             </div>
         </main>   
